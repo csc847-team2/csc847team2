454c6b0537dbb5bd619faa84aeab093a16c5f605
Umang Mathur <umangmathur10051992@gmail.com>
2018-11-03 17:47:36 -0700

    translation api now supports 4 languages

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,5 @@
+var currLangSelection = 'en';
+
 //On HTML Page load
 $(document).ready(function() {
     document.getElementById("fileInputBtn").addEventListener("change", readFile);
@@ -7,11 +9,12 @@ $(document).ready(function() {
     $("#translateBtn").click(function() {
         fetchTranslationFromServer();
     });
+    populateLanguageDropdownList();
 });
 
 function fetchTranslationFromServer() {
     var inputTextStr = document.getElementById("outputTxt").innerHTML;
-    var langCode = 'ru';
+    var langCode = currLangSelection;
     const body = {
         inputStr: inputTextStr,
         languageCode: langCode,
@@ -62,4 +65,50 @@ function readFile() {
         }); 
         FR.readAsDataURL( this.files[0] );
     }
+}
+
+// TODO: Rewrite this method. Code is too repititive.
+function populateLanguageDropdownList() {
+    var languageDropdownList = document.getElementById('languageDropdown');
+    let anchorElement = document.createElement('a');
+        anchorElement.setAttribute('class', 'dropdown-item');
+        anchorElement.setAttribute('onmouseover', '');
+        anchorElement.setAttribute('style', 'cursor: pointer;');
+        anchorElement.innerHTML = 'English';
+        anchorElement.addEventListener("click", function (event) {
+            $("#btnLanguageDropdown").text('English');
+            currLangSelection = 'en';
+        });
+        languageDropdownList.appendChild(anchorElement);
+        let anchorElement2 = document.createElement('a');
+        anchorElement2.setAttribute('class', 'dropdown-item');
+        anchorElement2.setAttribute('onmouseover', '');
+        anchorElement2.setAttribute('style', 'cursor: pointer;');
+        anchorElement2.innerHTML = 'Hindi';
+        anchorElement2.addEventListener("click", function (event) {
+            $("#btnLanguageDropdown").text('Hindi');
+            currLangSelection = 'hi';
+        });
+        languageDropdownList.appendChild(anchorElement2);
+        let anchorElement3 = document.createElement('a');
+        anchorElement3.setAttribute('class', 'dropdown-item');
+        anchorElement3.setAttribute('onmouseover', '');
+        anchorElement3.setAttribute('style', 'cursor: pointer;');
+        anchorElement3.innerHTML = 'Russian';
+        anchorElement3.addEventListener("click", function (event) {
+            $("#btnLanguageDropdown").text('Russian');
+            currLangSelection = 'ru';
+        });
+        languageDropdownList.appendChild(anchorElement3);
+        let anchorElement4 = document.createElement('a');
+        anchorElement4.setAttribute('class', 'dropdown-item');
+        anchorElement4.setAttribute('onmouseover', '');
+        anchorElement4.setAttribute('style', 'cursor: pointer;');
+        anchorElement4.innerHTML = 'Spanish';
+        anchorElement4.addEventListener("click", function (event) {
+            $("#btnLanguageDropdown").text('Spanish');
+            currLangSelection = 'es';
+        });
+        languageDropdownList.appendChild(anchorElement4);
+    return anchorElement;
 }
