725d9b0732d12e563cf1d6ad6a9ddecc524bac5d
sushilkplassar <sushilkplassar@gmail.com>
2018-11-06 19:23:43 -0800

    textToSpeech Implementation

---
+++ mp3.js
@@ -0,0 +1,20 @@
+var http = require('http'),
+    fileSystem = require('fs'),
+    path = require('path'),
+    util = require('util');
+
+http.createServer(function(request, response) {
+    var filePath = 'output.mp3';
+    var stat = fileSystem.statSync(filePath);
+
+    response.writeHead(200, {
+        'Content-Type': 'audio/mpeg',
+        'Content-Length': stat.size
+    });
+
+    var readStream = fileSystem.createReadStream(filePath);
+    // We replaced all the event handlers with a simple call to util.pump()
+    // util.pump(readStream, response);
+    readStream.pipe(response);
+})
+.listen(2000);
---
+++ routes/textToSpeech_Org.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+
+// Imports the Google Cloud client library
+const textToSpeech = require('@google-cloud/text-to-speech');
+
+// Creates a client
+const client = new textToSpeech.TextToSpeechClient();
+
+// The text to synthesize
+const text = 'Welcome to CONCORD CALIFORNIA...!';
+
+// Construct the request
+const request = {
+  input: {text: text},
+  // Select the language and SSML Voice Gender (optional)
+  voice: {languageCode: 'en-US', ssmlGender: 'NEUTRAL'},
+  // Select the type of audio encoding
+  audioConfig: {audioEncoding: 'MP3'},
+};
+
+// Performs the Text-to-Speech request
+client.synthesizeSpeech(request, (err, response) => {
+  if (err) {
+    console.error('ERROR:', err);
+    return;
+  }
+
+  // Write the binary audio content to a local file
+  fs.writeFile('txtSpeechOutput.mp3', response.audioContent, 'binary', err => {
+    if (err) {
+      console.error('ERROR:', err);
+      return;
+    }
+    console.log('Audio content written to file: txtSpeechOutput.mp3');
+  });
+
+  fs.readFile('./routes/txtSpeechOutput.mp3', 'utf8', function(err, contents) {
+    console.log(contents);
+});
+console.log('after calling readFile');
+});
